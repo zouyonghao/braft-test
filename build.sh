8436e1f045ddff99014ac780521371949600b1ae
Yong-Hao Zou <yonghaoz1994@gmail.com>
2022-07-27 17:03:27 +0800

    Use gcc-9

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 # export CC=/home/zyh/distributed-system-test/build/fuzz/default_compiler
 # export CXX=/home/zyh/distributed-system-test/build/fuzz/default_compiler++
 
-# cp /home/zyh/distributed-system-test/configs/*.json /tmp/
+# cp /home/zyh/distributed-system-test/configs/compiler-config.json /tmp/
 # cp /home/zyh/distributed-system-test/build/fuzz/CMakeFiles/default_instrument_lib.dir/instrumentor_lib/default_instrument_lib.c.o /tmp
 
-export CC="clang-9"
-export CXX="clang++-9"
+# export CC="clang-9"
+# export CXX="clang++-9"
+export CC=gcc-9
+export CXX=gcc-9
 
 rm -rf build
 
